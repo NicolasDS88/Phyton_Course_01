8dd0787c48e75f4b733a48fd1788d23723e66de6
Nicolas da Silva <39927474+NicolasDS88@users.noreply.github.com>
2021-09-01 13:20:26 -0300

    Create Lesson_1_Control_Flow.py

---
+++ Lesson_1_Control_Flow.py
@@ -0,0 +1,43 @@
+'''
+Created on Mar 27, 2017
+
+'''
+
+# First lesson how to use:
+# Multiple choices - List, Array and Dictionary
+# Flow control - For loop
+# Conditional sentences
+
+
+def main():
+    
+    print('This is how a Dictionary is use it')
+    choices=dict(
+        alex="A",
+        jane="B",
+        john="C"
+        )
+    print(choices["alex"])
+    
+    print('This is how flow control is use it for FOR sentences')
+    
+    data="This is where I want to break"
+    for char in data:
+        if char=='b': break
+        print(char,end='')
+    
+    print("") # Write a blank line
+    print('This is how conditional sentences is use it for ')
+    
+    x,y=10,1000
+    if (x==10):
+        msg="x is equal to 10"
+    elif (x<y):
+        msg="x is less than y"
+    elif (x==y):
+        msg="x is equal to y"
+    else:
+        msg="x is greater than y"
+    print(msg)
+      
+main()
